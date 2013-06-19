51934db5344036ae23a44cf4ded7c87e7031edee
Stefan Daschek <stefan@die-antwort.eu>
2013-06-19 17:30:48 +0200

    Do not echo rspec command line in textmate output window.
    The idea behind this feature was to allow easily rerunning the same command in the terminal by just copy-pasting. This is no longer necessary now that there is 'Run Context in Terminal' :-)

--- Support/lib/rspec/da-mate.rb
+++ Support/lib/rspec/da-mate.rb
@@ -6,12 +6,12 @@ def run_rspec(*args)
   seed = rand(65535)
   args += %W(--format textmate --order rand:#{seed})
   if binstub_available?
-    run_with_echo("bin/rspec", *args)
+    system("bin/rspec", *args)
   elsif zeus_available?
-    run_with_echo("zeus", "rspec", *args)
+    system("zeus", "rspec", *args)
   else
     puts "Neither binstubs nor zeus available, falling back to bundle exec ...<br>"
-    run_with_echo("bundle", "exec", "rspec", *args)
+    system("bundle", "exec", "rspec", *args)
   end
 end
 
@@ -46,11 +46,6 @@ end
 
 def load_last_run_args
   Marshal.load(File.read(LAST_RUN_FILENAME))
-end
-
-def run_with_echo(*args)
-  puts "<pre style='font-size: 11px'>" + args.map{ |a| a.sub(Dir.pwd, ".") }.join(" ") + "</pre>"
-  system *args
 end
 
 def binstub_available?
