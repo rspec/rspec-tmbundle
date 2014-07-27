bdbf2155be6671914faf8ca73075d252d3e38e78
Stefan Daschek <stefan@die-antwort.eu>
2014-07-27 18:54:43 +0200

    Fix deprecation (and formatter problem) in RSpec 2.14
    RSpec 2.14 has both `backtrace_clean_patterns` and `backtrace_exclusion_patterns`. Using the former generates a deprecation message, which somehow also disables the textmate formatter.

--- Support/lib/rspec/mate/filter_bundle_backtrace.rb
+++ Support/lib/rspec/mate/filter_bundle_backtrace.rb
@@ -1,8 +1,8 @@
 if defined?(RSpec)
   bundle_patterns = [%r{/RSpec\.tmbundle/}, %r{^/tmp/textmate-command}]
-  if RSpec.configuration.respond_to?(:backtrace_clean_patterns)
-    RSpec.configuration.backtrace_clean_patterns += bundle_patterns
-  elsif RSpec.configuration.respond_to?(:backtrace_exclusion_patterns)
+  if RSpec.configuration.respond_to?(:backtrace_exclusion_patterns)
     RSpec.configuration.backtrace_exclusion_patterns += bundle_patterns
+  elsif RSpec.configuration.respond_to?(:backtrace_clean_patterns)
+    RSpec.configuration.backtrace_clean_patterns += bundle_patterns
   end
 end
