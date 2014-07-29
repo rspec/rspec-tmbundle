0020bd692177b0885240ad3f71de9d1fd9cbf8f8
Stefan Daschek <stefan@die-antwort.eu>
2014-07-29 22:10:58 +0200

    Remove redundant specs
    `expect_twins` already checks both directions, so no need to repeat examples with reversed arguments.

--- Support/spec/rspec/mate/switch_command_spec.rb
+++ Support/spec/rspec/mate/switch_command_spec.rb
@@ -83,28 +83,13 @@ EOF
         ]
 
         expect_twins [
-          "/a/full/path/spec/controllers/application_controller_spec.rb",
-          "/a/full/path/app/controllers/application_controller.rb"
-        ]
-
-        expect_twins [
           "/a/full/path/app/controllers/job_applications_controller.rb",
           "/a/full/path/spec/controllers/job_applications_controller_spec.rb"
         ]
 
         expect_twins [
-          "/a/full/path/spec/controllers/job_applications_controller_spec.rb",
-          "/a/full/path/app/controllers/job_applications_controller.rb"
-        ]
-
-        expect_twins [
           "/a/full/path/app/helpers/application_helper.rb",
           "/a/full/path/spec/helpers/application_helper_spec.rb"
-        ]
-
-        expect_twins [
-          "/a/full/path/spec/helpers/application_helper_spec.rb",
-          "/a/full/path/app/helpers/application_helper.rb"
         ]
 
         expect_twins [
