af3f38a909b1a6263087ac406ceb56fe874819dc
Stefan Daschek <stefan@die-antwort.eu>
2014-08-08 16:50:53 +0200

    Extract method `build_argv_from_options`

--- Support/lib/rspec/mate/runner.rb
+++ Support/lib/rspec/mate/runner.rb
@@ -38,28 +38,8 @@ module RSpec
         stderr     = StringIO.new
         old_stderr = $stderr
         $stderr    = stderr
-        default_formatter = rspec3? ? 'RSpec::Mate::Formatters::TextMateFormatter' : 'textmate'
-        formatter  = ENV['TM_RSPEC_FORMATTER'] || default_formatter
 
-        if rspec3?
-          # If :line is given, only the first file from :files is used. This should be ok though, because
-          # :line is only ever set in #run_focussed, and there :files is always set to a single file only.
-          argv = options[:line] ? ["#{options[:files].first}:#{options[:line]}"] : options[:files].dup
-        else
-          argv = options[:files].dup
-          if options[:line]
-            argv << '--line'
-            argv << options[:line]
-          end
-        end
-
-        argv << '--format' << formatter
-        argv << '-r' << File.join(File.dirname(__FILE__), 'text_mate_formatter') if formatter == 'RSpec::Mate::Formatters::TextMateFormatter'
-        argv << '-r' << File.join(File.dirname(__FILE__), 'filter_bundle_backtrace')
-
-        if ENV['TM_RSPEC_OPTS']
-          argv += ENV['TM_RSPEC_OPTS'].split(" ")
-        end
+        argv = build_argv_from_options(options)
 
         Dir.chdir(project_directory) do
           if use_binstub?
@@ -103,6 +83,33 @@ module RSpec
 
     private
 
+      def build_argv_from_options(options)
+        default_formatter = rspec3? ? 'RSpec::Mate::Formatters::TextMateFormatter' : 'textmate'
+        formatter  = ENV['TM_RSPEC_FORMATTER'] || default_formatter
+
+        if rspec3?
+          # If :line is given, only the first file from :files is used. This should be ok though, because
+          # :line is only ever set in #run_focussed, and there :files is always set to a single file only.
+          argv = options[:line] ? ["#{options[:files].first}:#{options[:line]}"] : options[:files].dup
+        else
+          argv = options[:files].dup
+          if options[:line]
+            argv << '--line'
+            argv << options[:line]
+          end
+        end
+
+        argv << '--format' << formatter
+        argv << '-r' << File.join(File.dirname(__FILE__), 'text_mate_formatter') if formatter == 'RSpec::Mate::Formatters::TextMateFormatter'
+        argv << '-r' << File.join(File.dirname(__FILE__), 'filter_bundle_backtrace')
+
+        if ENV['TM_RSPEC_OPTS']
+          argv += ENV['TM_RSPEC_OPTS'].split(" ")
+        end
+        
+        argv
+      end
+      
       def last_remembered_single_file
         file = File.read(LAST_REMEMBERED_FILE_CACHE).strip
 
