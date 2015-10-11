b445a80fbd13e95c22752a9cedc4f8172bec67a6
Stefan Daschek <stefan@daschek.net>
2015-10-11 02:37:24 +0200

    Remove RSpec::Mate::Options.
    No longer used.

--- Support/lib/rspec/mate.rb
+++ Support/lib/rspec/mate.rb
@@ -5,7 +5,6 @@ ENV['TM_PROJECT_DIRECTORY'] ||= File.dirname(ENV['TM_FILEPATH'])
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/..')
 
 require 'rspec/mate/runner'
-require 'rspec/mate/options'
 require 'rspec/mate/switch_command'
 
 def gemfile?
