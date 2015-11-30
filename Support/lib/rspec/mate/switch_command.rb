rspec/rspec-tmbundle

commit 41cf1b2cf84a9579c04da658857560ad1ec20551

Implement TM_RSPEC_BASEDIR

Support/lib/rspec/mate/switch_command.rb
@@ -139,7 +139,7 @@ module RSpec
 
       def write_and_open(path)
         FileUtils.mkdir_p(File.dirname(path))
-        described = described_class_for(path, ENV['TM_PROJECT_DIRECTORY'])
+        described = described_class_for(path, base_dir)
         File.open(path, 'w') do |f|
           f.puts "require 'spec_helper'"
           f.puts ''

Support/fixtures/project_with_binstub/subdir/bin/rspec
@@ -1,0 +1,3 @@
+#!/bin/bash
+
+[ $1 == '--version' ] && echo "2.99.1-subdir-fake"

Support/lib/rspec/mate/gutter_marks.rb
@@ -1,8 +1,11 @@
 require "set"
+require_relative "helpers"
 
 module RSpec
   module Mate
     class GutterMarks
+      include Helpers
+      
       MARK = "warning"
       SpecError = Struct.new(:line, :message)
 
@@ -13,7 +16,7 @@ module RSpec
       end
 
       def set_marks
-        Dir.chdir(ENV['TM_PROJECT_DIRECTORY'] || ENV['TM_DIRECTORY']) do # Paths are relative to project directory
+        Dir.chdir(base_dir) do # Paths are relative to project directory
           errors_by_path.each do |path, errors|
             clear_marks_for(path)
             errors.each do |error|

Support/lib/rspec/mate/helpers.rb
@@ -1,0 +1,9 @@
+module RSpec
+  module Mate
+    module Helpers
+      def base_dir
+        ENV['TM_RSPEC_BASEDIR'] || ENV['TM_PROJECT_DIRECTORY'] || File.dirname(ENV['TM_FILEPATH'])
+      end
+    end
+  end
+end

Support/lib/rspec/mate/runner.rb
@@ -3,10 +3,12 @@ require 'cgi'
 require 'shellwords'
 require 'open3'
 require 'yaml'
+require_relative 'helpers'
 
 module RSpec
   module Mate
     class Runner
+      include Helpers
       LAST_REMEMBERED_FILE_CACHE = "/tmp/textmate_rspec_last_remembered_file_cache.txt"
       LAST_RUN_CACHE             = "/tmp/textmate_rspec_last_run.yml"
       
@@ -100,15 +102,15 @@ module RSpec
       def rspec_version
         @rspec_version ||= begin
           version = if gemfile?
-            specs = File.readlines(File.join(ENV['TM_PROJECT_DIRECTORY'], 'Gemfile.lock'))
+            specs = File.readlines(File.join(base_dir, 'Gemfile.lock'))
             # RegExp taken from https://github.com/bundler/bundler/blob/master/lib/bundler/lockfile_parser.rb
             specs.detect{ |line| line.match(%r{^ {4}rspec-core(?: \(([^-]*)(?:-(.*))?\))?$}) } && $1 or raise "'rspec' not found in Gemfile.lock!"
           elsif use_binstub?
-            Dir.chdir(ENV["TM_PROJECT_DIRECTORY"]) do
+            Dir.chdir(base_dir) do
               `bin/rspec --version`.chomp
             end
           else
-            Dir.chdir(ENV["TM_PROJECT_DIRECTORY"]) do
+            Dir.chdir(base_dir) do
               `rspec --version`.chomp
             end
           end
@@ -178,7 +180,7 @@ module RSpec
       end
 
       def project_directory
-        File.expand_path(ENV['TM_PROJECT_DIRECTORY']) rescue File.dirname(single_file)
+        File.expand_path(base_dir) rescue File.dirname(single_file)
       end
 
       def single_file

Support/spec/rspec/mate/runner_spec.rb
@@ -200,6 +200,21 @@ describe RSpec::Mate::Runner do
         expect(@spec_mate.rspec_version).to eq "2.99.1-fake"
       end
     end
+    
+    context 'when TM_RSPEC_BASEDIR is set' do
+      it 'looks there for the Gemfile.lock' do
+        ENV["TM_PROJECT_DIRECTORY"] = fixtures_path("project_with_gemfile")
+        ENV["TM_RSPEC_BASEDIR"] = fixtures_path("project_with_gemfile") + "/subdir"
+        expect(@spec_mate.rspec_version).to eq "3.3.0"
+      end
+
+      it 'looks there for the binstub' do
+        ENV["TM_PROJECT_DIRECTORY"] = fixtures_path("project_with_binstub")
+        ENV["TM_RSPEC_BASEDIR"] = fixtures_path("project_with_binstub") + "/subdir"
+        expect(@spec_mate.rspec_version).to eq "2.99.1-subdir-fake"
+      end
+    end
+    
   end
 
 private

commit 3e482d8ce01637f0a989aec420b6c5855bc01864

Fix error in “Alternate File” command.
Caused by 41cf1b2c. Fixes #104

Support/lib/rspec/mate/switch_command.rb
@@ -1,10 +1,13 @@
 require "fileutils"
+require File.dirname(__FILE__) + "/helpers.rb"
 
 module RSpec
   module Mate
     # This is based on Ruy Asan's initial code:
     # http://ruy.ca/posts/6-A-simple-switch-between-source-and-spec-file-command-for-textmate-with-auto-creation-
     class SwitchCommand
+      include Helpers
+      
       def go_to_twin(project_directory, filepath)
         other = twin(filepath)
 